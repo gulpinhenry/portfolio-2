2ee37861955326877b16a5174d8373832d4b6da3
Henry Kam <h3nrykam@gmail.com>
2021-10-10 16:18:42 -0700

    implement parallax features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-/*UIkit.cover(element, options);
-UIkit.parallax(element, options);
-UIkit.scroll(element, options);
-UIkit.img(element, options);*/
+// UIkit.cover(element, options);
+// UIkit.parallax(element, options);
+// UIkit.scroll(element, options);
+// UIkit.img(element, options);
+//UIkit.scrollspy(element, options);
